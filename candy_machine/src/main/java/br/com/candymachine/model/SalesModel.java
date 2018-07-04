8b5adb5fe872cdee564307632c7a7a99a8427d0a
Gustavo <gustavocalandrini@live.com>
2018-07-04 20:06:58 -0300

    Registrando produtos

--- candy_machine/src/main/java/br/com/candymachine/model/SalesModel.java
+++ candy_machine/src/main/java/br/com/candymachine/model/SalesModel.java
@@ -3,43 +3,52 @@
  */
 package br.com.candymachine.model;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
- 
-@Table(name="sales")
+
+@Table(name = "sales")
 @Entity
 public class SalesModel {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name="id_sales")
+	@Column(name = "id_sales")
 	private Integer id;
- 
-	@Column(name="quantity")
-	private String  quantity;
 
-	@Column(name="value")
-	private float  value;
-	
-	@Column(name="change")
+	@Column(name = "id_product")
+	private int idProduct;
+
+	@Column(name = "quantity")
+	private String quantity;
+
+	@Column(name = "value")
+	private float value;
+
+	@Column(name = "`change`")
 	private float change;
-	
-	@Column(name="final_value")
-	private float  final_value;
-	
-	public SalesModel() {}
-	
+
+	@Column(name = "final_value")
+	private float finalValue;
+
+	@Column(name = "date")
+	private Date date;
+
+	public SalesModel() {
+	}
+
 	public SalesModel(Integer id, String quantity, float value, float change, float final_value) {
 		super();
 		this.id = id;
 		this.quantity = quantity;
 		this.value = value;
 		this.change = change;
-		this.final_value = final_value;
+		this.finalValue = final_value;
 	}
 
 	public Integer getId() {
@@ -48,6 +57,22 @@ public class SalesModel {
 
 	public void setId(Integer id) {
 		this.id = id;
+	}
+
+	public int getIdProduct() {
+		return idProduct;
+	}
+
+	public void setIdProduct(int idProduct) {
+		this.idProduct = idProduct;
+	}
+
+	public float getValue() {
+		return value;
+	}
+
+	public void setValue(float value) {
+		this.value = value;
 	}
 
 	public String getQuantity() {
@@ -67,15 +92,25 @@ public class SalesModel {
 	}
 
 	public float getFinal_value() {
-		return final_value;
+		return finalValue;
 	}
 
-	public void setFinal_value(float final_value) {
-		this.final_value = final_value;
+	public void setFinal_value(float finalValue) {
+		this.finalValue = finalValue;
 	}
 
-	
-	
- 
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	@Override
+	public String toString() {
+		return "SalesModel [id=" + id + ", idProduct=" + idProduct + ", quantity=" + quantity + ", value=" + value
+				+ ", change=" + change + ", finalValue=" + finalValue + ", date=" + date + "]";
+	}
+
 }
-
--- candy_machine/src/main/java/br/com/candymachine/service/SalesService.java
+++ candy_machine/src/main/java/br/com/candymachine/service/SalesService.java
@@ -1,5 +1,6 @@
 package br.com.candymachine.service;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,96 +16,98 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.candymachine.model.ResponseModel;
 import br.com.candymachine.model.SalesModel;
 import br.com.candymachine.repository.SalesRepository;
- 
+
 @Service
 @RestController
 @RequestMapping("/service")
 public class SalesService {
- 
+
 	@Autowired
-	private SalesRepository salesRepository; 
- 
+	private SalesRepository salesRepository;
+
 	/**
 	 * SALVAR UM NOVO REGISTRO
+	 * 
 	 * @param sale
 	 * @return
 	 */
-	@RequestMapping(value="/sales", method = RequestMethod.POST, consumes=MediaType.APPLICATION_JSON_UTF8_VALUE,produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public @ResponseBody ResponseModel insert(@RequestBody SalesModel sale){
- 
- 
+	@RequestMapping(value = "/sales", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public @ResponseBody ResponseModel insert(@RequestBody SalesModel sale) {
+		sale.setDate(new Date());
+		System.out.println("SALE: " + sale.toString());
 		try {
- 
 			this.salesRepository.save(sale);
- 
-			return new ResponseModel(1,"Registro salvo com sucesso!");
- 
-		}catch(Exception e) {
- 
-			return new ResponseModel(0,e.getMessage());			
-		}
-	}
- 
-	/**
-	 * ATUALIZAR O REGISTRO DE UMA PESSOA
-	 * @param sale
-	 * @return
-	 */
-	@RequestMapping(value="/sales", method = RequestMethod.PUT, consumes=MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public @ResponseBody ResponseModel update(@RequestBody SalesModel sale){
- 
-		try {
- 
-			this.salesRepository.save(sale);		
- 
-			return new ResponseModel(1,"Registro atualizado com sucesso!");
- 
-		}catch(Exception e) {
- 
-			return new ResponseModel(0,e.getMessage());
-		}
-	}
- 
-	/**
-	 * CONSULTAR TODAS AS PESSOAS
-	 * @return
-	 */
-	@RequestMapping(value="/sales", method = RequestMethod.GET, produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public @ResponseBody List<SalesModel> select(){
- 
-		return this.salesRepository.findAll();
-	}
- 
-	/**
-	 * BUSCAR UMA PESSOA PELO CÓDIGO
-	 * @param id
-	 * @return
-	 */
-	@RequestMapping(value="/sales/{id}", method = RequestMethod.GET, produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public @ResponseBody SalesModel find(@PathVariable("id") Integer id){
- 
-		return this.salesRepository.findById(id);
-	}
- 
-	/***
-	 * EXCLUIR UM REGISTRO PELO CÓDIGO
-	 * @param id
-	 * @return
-	 */
-	@RequestMapping(value="/sales/{id}", method = RequestMethod.DELETE, produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public @ResponseBody ResponseModel delete(@PathVariable("id") Integer id){
- 
-		SalesModel salesModel = salesRepository.findById(id);
- 
-		try {
- 
-			salesRepository.delete(salesModel);
- 
-			return new ResponseModel(1, "Registro excluido com sucesso!");
- 
-		}catch(Exception e) {
+			return new ResponseModel(1, "Registro salvo com sucesso!");
+
+		} catch (Exception e) {
 			return new ResponseModel(0, e.getMessage());
 		}
 	}
- 
+
+	/**
+	 * ATUALIZAR O REGISTRO DE UMA PESSOA
+	 * 
+	 * @param sale
+	 * @return
+	 */
+	@RequestMapping(value = "/sales", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public @ResponseBody ResponseModel update(@RequestBody SalesModel sale) {
+
+		try {
+
+			this.salesRepository.save(sale);
+
+			return new ResponseModel(1, "Registro atualizado com sucesso!");
+
+		} catch (Exception e) {
+
+			return new ResponseModel(0, e.getMessage());
+		}
+	}
+
+	/**
+	 * CONSULTAR TODAS AS PESSOAS
+	 * 
+	 * @return
+	 */
+	@RequestMapping(value = "/sales", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public @ResponseBody List<SalesModel> select() {
+
+		return this.salesRepository.findAll();
+	}
+
+	/**
+	 * BUSCAR UMA PESSOA PELO CÓDIGO
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value = "/sales/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public @ResponseBody SalesModel find(@PathVariable("id") Integer id) {
+
+		return this.salesRepository.findById(id);
+	}
+
+	/***
+	 * EXCLUIR UM REGISTRO PELO CÓDIGO
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value = "/sales/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public @ResponseBody ResponseModel delete(@PathVariable("id") Integer id) {
+
+		SalesModel salesModel = salesRepository.findById(id);
+
+		try {
+
+			salesRepository.delete(salesModel);
+
+			return new ResponseModel(1, "Registro excluido com sucesso!");
+
+		} catch (Exception e) {
+			return new ResponseModel(0, e.getMessage());
+		}
+	}
+
 }
